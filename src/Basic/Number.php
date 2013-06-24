5cd4291b37b098f0222b9254a947208ba916becb
kpacha <kpacha666@gmail.com>
2013-06-25 00:06:11 +0300

    Adding the numerals for 40, 50, 400 & 500

--- src/Basic/Number.php
+++ src/Basic/Number.php
@@ -27,8 +27,12 @@ class Number
     protected $romanSymbols = [
         1000    => 'M',
         900     => 'CM',
+        500     => 'D',
+        400     => 'CD',
         100     => 'C',
         90      => 'XC',
+        50      => 'L',
+        40      => 'XL',
         10      => 'X',
         9       => 'IX',
         5       => 'V',
